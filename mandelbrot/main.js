7be66fc3a3178204b7b05889bd3c9d26155d1e62
Neil Hendren <neil.hendren@gmail.com>
2021-07-04 03:48:27 -0600

    attempt to improve performance and quality

--- mandelbrot/main.js
+++ mandelbrot/main.js
@@ -1,4 +1,4 @@
-let g = {
+window.g = {
   cnv : null,
   zoomSlider : document.getElementById("zoom-slider"),
   panXSlider : document.getElementById("pan-x"),
@@ -15,23 +15,20 @@ let g = {
   FOVMin : 1e-9,
   FOVMax : 4,
   max_iterations : 40,
-  pixel_array : [],
+  pixel_index : 0,
   is_panning : false,
 };
 
 function setup() {
-  g.cnv = createCanvas(600, 600);
-  g.cnv.parent(g.graphicsArea);
+  window.g.cnv = createCanvas(600, 600);
+  window.g.cnv.parent(window.g.graphicsArea);
   document.getElementsByTagName("main")[0].remove();
-  for( let i = 0; i < width; i++ ) {
-    g.pixel_array.push(new Array(height));
-  }
-  const FOVMaxLog = Math.log(g.FOVMax);
-  const FOVMinLog = Math.log(g.FOVMin);
-  g.defaultZoom = Math.log(g.FOV);
-  const sliderValue = map(g.defaultZoom, FOVMinLog, FOVMaxLog, 1, 0);
-  g.zoom = Math.log(g.FOV);
-  g.zoomSlider.value = `${sliderValue}`;
+  const FOVMaxLog = Math.log(window.g.FOVMax);
+  const FOVMinLog = Math.log(window.g.FOVMin);
+  window.g.defaultZoom = Math.log(window.g.FOV);
+  const sliderValue = map(window.g.defaultZoom, FOVMinLog, FOVMaxLog, 1, 0);
+  window.g.zoom = Math.log(window.g.FOV);
+  window.g.zoomSlider.value = `${sliderValue}`;
   background(255);
   pixelDensity(1);
   adjustZoom();
@@ -40,78 +37,66 @@ function setup() {
 }
 
 function draw() {
-  calculate();
+  window.g.pixel_index = 0;
   loadPixels();
-  let d = pixelDensity();
-  let index = 0;
-  for ( let i = 0; i < width; i++ ) {
-    for ( let j = 0; j < height; j++ ) {
-      for ( let m = 0; m < d; m++ ) {
-        pixels[index]     = g.pixel_array[j][i][0];
-        pixels[index + 1] = g.pixel_array[j][i][1];
-        pixels[index + 2] = g.pixel_array[j][i][2];
-        pixels[index + 3] = 255;
-        index += 4;
-      }
-    }
-  }
+  update();
   updatePixels();
 }
 
 function adjustZoom() {
   const speed = 0.15;
-  const value = Number(g.zoomSlider.value);
+  const value = Number(window.g.zoomSlider.value);
   const dZoom = value * speed;
-  const newZoom = g.zoom - dZoom;
-  const FOVMaxLog = Math.log(g.FOVMax);
-  const FOVMinLog = Math.log(g.FOVMin);
-  const maxIterationFar = 50;
-  const maxIterationZoomed = 400;
+  const newZoom = window.g.zoom - dZoom;
+  const FOVMaxLog = Math.log(window.g.FOVMax);
+  const FOVMinLog = Math.log(window.g.FOVMin);
+  const maxIterationFar = 40;
+  const maxIterationZoomed = 300;
   let newIterations = map(newZoom, FOVMaxLog, FOVMinLog, maxIterationFar, maxIterationZoomed);
   newIterations = Math.ceil(newIterations);
-  g.max_iterations = newIterations;
+  window.g.max_iterations = newIterations;
   if( newZoom < FOVMaxLog && newZoom > FOVMinLog ) {
-    g.zoom -= dZoom;
-    g.FOV = Math.exp(g.zoom);
-    const rangeXMin = g.X - g.FOV / 2;
-    const rangeXMax = g.X + g.FOV / 2;
-    const rangeYMin = g.Y - g.FOV / 2;
-    const rangeYMax = g.Y + g.FOV / 2;
-    g.range = [[rangeXMin, rangeXMax], [rangeYMin, rangeYMax]];
+    window.g.zoom -= dZoom;
+    window.g.FOV = Math.exp(window.g.zoom);
+    const rangeXMin = window.g.X - window.g.FOV / 2;
+    const rangeXMax = window.g.X + window.g.FOV / 2;
+    const rangeYMin = window.g.Y - window.g.FOV / 2;
+    const rangeYMax = window.g.Y + window.g.FOV / 2;
+    window.g.range = [[rangeXMin, rangeXMax], [rangeYMin, rangeYMax]];
   }
 
   redraw();
   updateLabels()
-  if(g.is_panning) {
+  if(window.g.is_panning) {
     window.setTimeout(adjustZoom, 100);
   }
 }
 
 function pan() {
   const speed = 0.05;
-  const panXValue = Number(g.panXSlider.value);
-  const panYValue = Number(g.panYSlider.value);
-  const dx = panXValue * speed * g.FOV;
-  const dy = panYValue * speed * g.FOV;
-  const incX = g.X + dx;
-  const incY = g.Y + dy;
-  const minX = g.maxRange[0][0] + g.FOV / 2;
-  const maxX = g.maxRange[0][1] - g.FOV / 2;
-  const minY = g.maxRange[1][0] + g.FOV / 2;
-  const maxY = g.maxRange[1][1] - g.FOV / 2;
+  const panXValue = Number(window.g.panXSlider.value);
+  const panYValue = Number(window.g.panYSlider.value);
+  const dx = panXValue * speed * window.g.FOV;
+  const dy = panYValue * speed * window.g.FOV;
+  const incX = window.g.X + dx;
+  const incY = window.g.Y + dy;
+  const minX = window.g.maxRange[0][0] + window.g.FOV / 2;
+  const maxX = window.g.maxRange[0][1] - window.g.FOV / 2;
+  const minY = window.g.maxRange[1][0] + window.g.FOV / 2;
+  const maxY = window.g.maxRange[1][1] - window.g.FOV / 2;
 
   if( incX > minX && incX < maxX && incY < maxY && incY > minY ) {
-    g.X = incX;
-    g.Y = incY;
-    g.range =
+    window.g.X = incX;
+    window.g.Y = incY;
+    window.g.range =
     [
-      [g.X - g.FOV / 2, g.X + g.FOV / 2],
-      [g.Y - g.FOV / 2, g.Y + g.FOV / 2]
+      [window.g.X - window.g.FOV / 2, window.g.X + window.g.FOV / 2],
+      [window.g.Y - window.g.FOV / 2, window.g.Y + window.g.FOV / 2]
     ];
   }
   redraw();
   updateLabels()
-  if(g.is_panning) {
+  if(window.g.is_panning) {
     window.setTimeout(pan, 100);
   }
 }
@@ -120,62 +105,62 @@ function updateLabels() {
   const dspan = document.getElementById("d-span");
   const panX = document.getElementById("pan-x-value");
   const panY = document.getElementById("pan-y-value");
-  const width = (g.range[0][1] - g.range[0][0]).toPrecision(2);
+  const width = (window.g.range[0][1] - window.g.range[0][0]).toPrecision(2);
   dspan.innerHTML = width;
-  panX.innerHTML = `Re = ${g.X.toFixed(2)}`;
-  panY.innerHTML = `Im = ${(-1 * g.Y).toFixed(2)}`;
+  panX.innerHTML = `Re = ${window.g.X.toFixed(2)}`;
+  panY.innerHTML = `Im = ${(-1 * window.g.Y).toFixed(2)}`;
 }
 
-g.zoomSlider.addEventListener("mousedown", function() {g.is_panning = true; adjustZoom() });
-g.panXSlider.addEventListener("mousedown", function() {g.is_panning = true; pan() });
-g.panYSlider.addEventListener("mousedown", function() {g.is_panning = true; pan() });
+window.g.zoomSlider.addEventListener("mousedown", function() {window.g.is_panning = true; adjustZoom() });
+window.g.panXSlider.addEventListener("mousedown", function() {window.g.is_panning = true; pan() });
+window.g.panYSlider.addEventListener("mousedown", function() {window.g.is_panning = true; pan() });
 
 // These 3 functions are all necessary because FIREFOX IS TRASH AND DOESN'T REGISTER MOUSEUP EVENTLISTENERS
 
 function mouseMoved() {
   if(!mouseIsPressed) {
-    g.zoomSlider.value = "0";
-    g.panXSlider.value = "0";
-    g.panYSlider.value = "0";
-    g.is_panning = false;
+    window.g.zoomSlider.value = "0";
+    window.g.panXSlider.value = "0";
+    window.g.panYSlider.value = "0";
+    window.g.is_panning = false;
   }
 };
 
 function mouseReleased() {
-  g.zoomSlider.value = "0";
-  g.panXSlider.value = "0";
-  g.panYSlider.value = "0";
-  g.is_panning = false;
+  window.g.zoomSlider.value = "0";
+  window.g.panXSlider.value = "0";
+  window.g.panYSlider.value = "0";
+  window.g.is_panning = false;
 };
 
 window.setInterval(() => {
   if(!mouseIsPressed) {
-    g.zoomSlider.value = "0";
-    g.panXSlider.value = "0";
-    g.panYSlider.value = "0";
-    g.is_panning = false;
+    window.g.zoomSlider.value = "0";
+    window.g.panXSlider.value = "0";
+    window.g.panYSlider.value = "0";
+    window.g.is_panning = false;
   }
 }, 200);
 
-g.resetButton.addEventListener("click", function() {
-  g.X = -0.5;
-  g.Y = 0;
-  g.range = [[-1.5, 0.5], [-1, 1]];
-  g.FOV = 2;
-  g.zoom = g.defaultZoom;
-  g.is_panning = false;
+window.g.resetButton.addEventListener("click", function() {
+  window.g.X = -0.5;
+  window.g.Y = 0;
+  window.g.range = [[-1.5, 0.5], [-1, 1]];
+  window.g.FOV = 2;
+  window.g.zoom = window.g.defaultZoom;
+  window.g.is_panning = false;
   pan();
   adjustZoom();
-  calculate();
+  update();
   redraw();
 })
 
-function calculate() {
-  for ( let i = 0; i < width; i++ ) {
-    for ( let j = 0; j < height; j++ ) {
-      let n = g.max_iterations;
-      let cx = g.range[0][0] + (i / width) * (g.range[0][1] - g.range[0][0]);
-      let cy = g.range[1][0] + (j / height) * (g.range[1][1] - g.range[1][0]);
+function update() {
+  for ( let j = 0; j < width; j++ ) {
+    for ( let i = 0; i < height; i++ ) {
+      let n = window.g.max_iterations;
+      const cx = window.g.range[0][0] + (i / width) * (window.g.range[0][1] - window.g.range[0][0]);
+      const cy = window.g.range[1][0] + (j / height) * (window.g.range[1][1] - window.g.range[1][0]);
       let x = 0;
       let y = 0;
       let xx = 0;
@@ -191,9 +176,13 @@ function calculate() {
         if( n <= 0 ) {break}
       }
 
-      const color = Math.floor(n / g.max_iterations * 255);
-      g.pixel_array[i][j] = [color, 127 + color / 2, 200 + 55 * (color / 255)];
-      
+      const color = n / g.max_iterations * 255;
+      pixels[ window.g.pixel_index ]    = color | 0;
+      pixels[ window.g.pixel_index + 1] = ( 127 + color / 2 ) | 0;
+      pixels[ window.g.pixel_index + 2] = ( 200 + 55 * (color / 255)) | 0;
+      pixels[ window.g.pixel_index + 3] = 255;
+
+      window.g.pixel_index += 4;
     }
   }
 }
