f3cead0ca8c781e71f69446a5db7d4f9fa978400
Neil Hendren <neil.hendren@gmail.com>
2021-09-13 04:32:10 -0600

    console log the player stats using heroku

--- dps_calc/fetch_stats.js
+++ dps_calc/fetch_stats.js
@@ -1,24 +1,38 @@
 const fetch_stats_button = document.getElementById("fetch-stats");
 
-const uri = encodeURIComponent();
-
 function fetch_stats() {
   let username = document.getElementById("display-name").value;
   if(username == "") return
   username = encodeURIComponent(username);
-  const URL = `https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=${username}`;
+  const URL = `https://neil-hendren-web-services.herokuapp.com/hiscores_request?username=${username}`;
 
-  var request = new Request(URL);
+  fetch(URL).then(resp => {
+    return resp.json();
+  }).then(json => {
+    const data = json.data;
+    const player_stats = {
+      attack : 1,
+      strength : 1,
+      defense : 1,
+      hitpoints : 10,
+      ranged : 1,
+      magic : 1,
+      prayer : 1,
+    };
+    if(data !== "bad request" && data !== undefined && typeof(data) !== "undefined" && data !== null) {
+      const player = JSON.parse(data);
+      const stats = player.main.skills;
+      player_stats.attack = Number(stats.attack.level);
+      player_stats.strength = Number(stats.strength.level);
+      player_stats.defense = Number(stats.defence.level);
+      player_stats.hitpoints = Number(stats.hitpoints.level);
+      player_stats.ranged = Number(stats.ranged.level);
+      player_stats.magic = Number(stats.magic.level);
+      player_stats.prayer = Number(stats.prayer.level);
+    }
 
-  fetch(request).then(function(response) {
-      // Convert to JSON
-      return response.json();
-  }).then(function(j) {
-      // Yay, `j` is a JavaScript object
-      console.log(JSON.stringify(j));
-  }).catch(function(error) {
-      console.log('Request failed', error)
-  });
+    console.log(player_stats)
+  })
 }
 
 fetch_stats_button.addEventListener("click", fetch_stats);
