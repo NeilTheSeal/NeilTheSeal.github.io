60814f53d964c7f87103d4b44c4488e9cb0a0999
Neil Hendren <neil.hendren@gmail.com>
2021-09-13 21:50:23 -0600

    fetch stats completed

--- dps_calc/fetch_stats.js
+++ dps_calc/fetch_stats.js
@@ -1,8 +1,19 @@
 const fetch_stats_button = document.getElementById("fetch-stats");
+const attack_input = document.getElementById("attack-level");
+const strength_input = document.getElementById("strength-level");
+const defense_input = document.getElementById("defense-level");
+const hitpoints_input = document.getElementById("hitpoints-level");
+const magic_input = document.getElementById("magic-level");
+const ranged_input = document.getElementById("ranged-level");
+const prayer_input = document.getElementById("prayer-level");
 
 function fetch_stats() {
+
   let username = document.getElementById("display-name").value;
-  if(username == "") return
+  if (username == "") return
+
+  fetch_stats_button.innerHTML = "Fetching . . .";
+  fetch_stats_button.setAttribute("disabled", "yes");
   username = encodeURIComponent(username);
   const URL = `https://neil-hendren-web-services.herokuapp.com/hiscores_request?username=${username}`;
 
@@ -11,15 +22,15 @@ function fetch_stats() {
   }).then(json => {
     const data = json.data;
     const player_stats = {
-      attack : 1,
-      strength : 1,
-      defense : 1,
-      hitpoints : 10,
-      ranged : 1,
-      magic : 1,
-      prayer : 1,
+      attack: 1,
+      strength: 1,
+      defense: 1,
+      hitpoints: 10,
+      ranged: 1,
+      magic: 1,
+      prayer: 1,
     };
-    if(data !== "bad request" && data !== undefined && typeof(data) !== "undefined" && data !== null) {
+    if (data !== "bad request" && data !== undefined && typeof (data) !== "undefined" && data !== null) {
       const player = JSON.parse(data);
       const stats = player.main.skills;
       player_stats.attack = Number(stats.attack.level);
@@ -31,7 +42,16 @@ function fetch_stats() {
       player_stats.prayer = Number(stats.prayer.level);
     }
 
-    console.log(player_stats)
+    fetch_stats_button.innerHTML = "Fetch Stats";
+    fetch_stats_button.removeAttribute("disabled");
+
+    attack_input.value = player_stats.attack;
+    strength_input.value = player_stats.strength;
+    defense_input.value = player_stats.defense;
+    hitpoints_input.value = player_stats.hitpoints;
+    magic_input.value = player_stats.magic;
+    ranged_input.value = player_stats.ranged;
+    prayer_input.value = player_stats.prayer;
   })
 }
 
